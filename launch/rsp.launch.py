4be42356fb9b0b54e79807a3f666cfdec82b4ee5
Artyks03 <horama57@gmail.com>
2024-10-21 17:10:03 +0200

    :D

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -15,7 +15,7 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
     use_ros2_control = LaunchConfiguration('use_ros2_control')
 
-    # Process the URDF file
+    # Process the URDF file.
     pkg_path = os.path.join(get_package_share_directory('robotek'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
     # robot_description_config = xacro.process_file(xacro_file)

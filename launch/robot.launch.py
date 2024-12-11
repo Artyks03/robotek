8b01324431111cdc1d9222215c6fc537fcf5dd81
Artyks03 <horama57@gmail.com>
2024-12-11 18:14:05 +0100

    Update robot.launch.py

--- launch/robot.launch.py
+++ launch/robot.launch.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Martin Horak
+# Copyright 2024 [Pravoplatny autor]
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
